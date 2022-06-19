7e5731b335ee3db61368acf21e92ff3ff7fd5b19
johan <djjordaan@gmail.com>
2022-06-19 17:52:42 +1000

    Working mulipage cards

--- src/app.js
+++ src/app.js
@@ -17,17 +17,38 @@ const psp2mm = (psp) => {
 //console.log(mm2psp(91),mm2psp(55))
 
 
-// Generate the uuid
-//
-const id =  '3fef46d5-7645-4a6d-80e8-b05bb43c7ecc' //uuid.v4();
-
-const cb = () => {
-    const id = uuid.v4();
-    qr.toFile(`./${id}.png`,id,{},(err,code)=>{
-        cb()
-    })
+const create_ids = (run_length, initial_ids) => {
+    const ids = initial_ids
+    for(let i=ids.length;i<run_length;i++) {
+        ids.push(uuid.v4())
+    }
+    return ids
 }
 
+const create_qrcodes = async (run_id,ids) => {
+    let id_idx = 0
+    let path = `../data/${run_id}`
+    fs.mkdirSync(path, { recursive: true })
+    let done = false
+    const cb = () => {
+        const id = ids[id_idx]
+        qr.toFile(`${path}/${id}.png`,id,{},(err,code)=>{
+            id_idx++;
+            if(id_idx<ids.length) {
+                cb()
+            } else {
+                done = true
+            }
+        })
+    }
+    cb()
+    while(!done) {
+        await new Promise(resolve => setTimeout(resolve, 2000));
+
+        let data = JSON.stringify({'run_id':run_id,'ids':ids},null,2);
+        fs.writeFileSync(`${path}/${run_id}.json`, data);
+    }
+}
 
 const cross = (doc, x,y) => {
     doc
@@ -38,11 +59,11 @@ const cross = (doc, x,y) => {
         .stroke();
 }
 
-const createCard = (doc,id,w,h,xOffset,yOffset) => {
+const createCard = (path,doc,id,w,h,xOffset,yOffset) => {
     const qrSize = mm2psp(40)
 
     doc
-        .image(`${id}.png`,mm2psp(15)+xOffset,mm2psp(13)+yOffset,{
+        .image(`${path}/${id}.png`,mm2psp(15)+xOffset,mm2psp(13)+yOffset,{
             fit:[qrSize,qrSize],
         });
 
@@ -74,57 +95,63 @@ const createCard = (doc,id,w,h,xOffset,yOffset) => {
     cross(doc,xOffset+w,yOffset);
 }
 
+const create_pdf = (run_id) => {
+    let path = `../data/${run_id}`
 
-// Generate the qr code and save it
-//
-qr.toFile(`./${id}.png`,id,{},(err,code)=>{
     const w = mm2psp(91);
     const h = mm2psp(55);
     const xOffset = mm2psp(10);
     const yOffset = mm2psp(10);
 
-    if(err) return console.log("error occurred")
+    const doc = new PDFDocument({size: 'A4'})//, layout:'landscape'});
+    doc.pipe(fs.createWriteStream(`${path}/${run_id}.pdf`));
 
-    //
-    const doc = new PDFDocument({size: 'A4', layout:'landscape'});
-    doc.pipe(fs.createWriteStream(`${id}.pdf`));
+    const filenames = fs.readdirSync(path);
 
-
-    createCard(
-        doc,
-        id,
-        w,h,
-        xOffset,
-        yOffset
-    );
-
-    createCard(
-        doc,
-        id,
-        w,h,
-        xOffset*2+w*1,
-        yOffset
-    );
-
-    createCard(
-        doc,
-        id,
-        w,h,
-        xOffset,
-        yOffset*2+h*1
-    );
-
-    createCard(
-        doc,
-        id,
-        w,h,
-        xOffset,
-        yOffset*3+h*2
-    );
-
-
-
+    let x=0
+    let y=0
+    filenames.forEach((filename)=>{
+        if(filename.endsWith('.png')) {
+            const id = filename.replace(".png","")
+            createCard(
+                path,
+                doc,
+                id,
+                w,h,
+                xOffset*(x+1)+(w*x),
+                yOffset*(y+1)+(h*y),
+            );
+            x = x + 1
+            if(x == 2) {
+                x = 0
+                y = y + 1
+            }
+            if(y == 4) {
+                x = 0
+                y = 0
+                doc.addPage()
+            }
+        }
+    })
 
     doc.end();
-})
+}
 
+const run = async (run_length, run_id, initial_ids, generate_pdf_only) => {
+    if(generate_pdf_only) {
+    } else {
+        const ids = create_ids(run_length,initial_ids)
+        await create_qrcodes(run_id, ids)
+    }
+    create_pdf(run_id)
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+const run_length = 20
+const run_id = 'fbe9d95d-72a5-48a6-95b6-86fe4fdf39e6' || uuid.v4()
+const initial_ids = [
+    '3fef46d5-7645-4a6d-80e8-b05bb43c7ecc'
+] || []
+run(run_length, run_id, initial_ids, false)
